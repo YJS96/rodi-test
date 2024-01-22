0bb1b2db2553e6b02c84e942d1b00af2f9b6da67
YJS96 <wnt9864@hotmail.com>
2024-01-22 14:54:20 +0900

    font

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ export default function Login() {
   return (
     <>
       {/* PC화면 */}
-      <div className="pc-background hidden md:flex w-screen h-screen justify-center items-center bg-[url('/images/login_bg.png')] bg-cover">
+      <div className="pc-background hidden md:flex w-screen h-screen font-Pretendard justify-center items-center bg-[url('/images/login_bg.png')] bg-cover">
         <div className="logo-frame flex flex-col w-[500px] justify-center items-center bg-color-white py-16 px-12 rounded-2xl drop-shadow-login">
           <Image src="/images/logo_vertical.svg" width="114" height="83" alt="main-logo"/>
           <div className="mt-7 text-lg font-semibold">
@@ -26,7 +26,7 @@ export default function Login() {
       </div>
 
       {/* 모바일화면 */}
-      <div className="md:hidden w-screen h-screen flex justify-center items-center">
+      <div className="md:hidden font-Pretendard w-screen h-screen flex justify-center items-center">
         <div className="flex flex-col items-center mb-44">
           <Image src="/images/logo_vertical.svg" width="114" height="83" alt="main-logo"/>
           <div className="mt-7 text-lg font-semibold">
